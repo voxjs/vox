c84f990f8e272d1cf49c6e8a7c34642867d0a219
Paula <paula.la@live.com>
2021-11-05 16:26:29 +0200

    update evaluator

--- src/evaluator.js
+++ src/evaluator.js
@@ -5,7 +5,11 @@ const cache = map();
 const evaluator = (expression) => (
   cache[expression] || (
     cache[expression] = new Function(
-      `with(this)return(${expression})`
+      'with(this)return' + (
+        (expression)
+          ? `(${expression})`
+          : ';'
+      )
     )
   )
 );
